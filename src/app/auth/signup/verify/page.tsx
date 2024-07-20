484624fed967ce1a67898c22f7bbbe1c17e2358a
deepansh <mauryadeepansh40@gmail.com>
2024-07-21 01:23:58 +0530

    build correction

--- src/app/auth/signup/verify/page.tsx
+++ src/app/auth/signup/verify/page.tsx
@@ -3,7 +3,7 @@ import { Toaster, toast } from "react-hot-toast";
 import InputBoxes from "components/InputBoxes";
 import Navbar from "components/Navbar";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useState } from "react";
 import { verifyEmail } from "~/app/actions/auth";
 const VerifyPage = () => {
   const [code, setCode] = useState<string[]>([]);
@@ -41,34 +41,36 @@ const VerifyPage = () => {
   };
 
   return (
-    <div>
-      <Navbar />
-      <main className="flex items-center justify-center mt-7 w-[100vw] ">
-        <form
-          onSubmit={handleVerifyCode}
-          className="flex flex-col items-center justify-center w-[526px] border border-slate-400  h-[420px] rounded-xl gap-5 mt-8"
-        >
-          <h1 className="text-[32px] font-bold">Verify your email</h1>
-          <h3 className="text-[18px] flex flex-col items-center justify-center ">
-            Enter the 8 digit code you have received on
-            <p className="font-semibold">{email}</p>
-          </h3>
-          <div className="flex flex-col  font-medium w-[80%] ">
-            <label htmlFor="email">Code</label>
-            <div className="flex justify-between gap-1 items-center">
-              <InputBoxes setCode={setCode} />
-            </div>
-          </div>
-          <button
-            className=" border text-xl mt-5  font-medium w-[80%] border-slate-400 rounded-md h-[50px] text-white bg-black"
-            type="submit"
+    <Suspense fallback={<div>loading....</div>}>
+      <div>
+        <Navbar />
+        <main className="flex items-center justify-center mt-7 w-[100vw] ">
+          <form
+            onSubmit={handleVerifyCode}
+            className="flex flex-col items-center justify-center w-[526px] border border-slate-400  h-[420px] rounded-xl gap-5 mt-8"
           >
-            {spinner == "spinner" ? "Verify" : "wait ..."}
-          </button>
-        </form>
-      </main>
-      <Toaster />
-    </div>
+            <h1 className="text-[32px] font-bold">Verify your email</h1>
+            <h3 className="text-[18px] flex flex-col items-center justify-center ">
+              Enter the 8 digit code you have received on
+              <p className="font-semibold">{email}</p>
+            </h3>
+            <div className="flex flex-col  font-medium w-[80%] ">
+              <label htmlFor="email">Code</label>
+              <div className="flex justify-between gap-1 items-center">
+                <InputBoxes setCode={setCode} />
+              </div>
+            </div>
+            <button
+              className=" border text-xl mt-5  font-medium w-[80%] border-slate-400 rounded-md h-[50px] text-white bg-black"
+              type="submit"
+            >
+              {spinner == "spinner" ? "Verify" : "wait ..."}
+            </button>
+          </form>
+        </main>
+        <Toaster />
+      </div>
+    </Suspense>
   );
 };
 
